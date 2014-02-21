e884524687ad347a429bc86758b2f73bb0afbb1f
James Halliday <mail@substack.net>
2014-02-21 15:26:15 -0800

    bundle files written

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var detect = require('parcel-detector');
 var mapper = require('parcel-map');
 var fs = require('fs');
 var path = require('path');
@@ -7,18 +6,50 @@ var match = require('minimatch');
 var shasum = require('shasum');
 var EventEmitter = require('events').EventEmitter;
 var mkdirp = require('mkdirp');
+var through2 = require('through2');
 var path = require('path');
 var fs = require('fs');
+var crypto = require('crypto');
+var concat = require('concat-stream');
 
-module.exports = function (b, opts, cb) {
+module.exports = function (b, opts) {
     mkdirp(opts.dst, function () {
         parcelMap(b, opts, function (err, map) {
-            if (err) return cb(err);
+            if (err) return output.emit('error', err);
             withMap(map);
         });
+        var ostream = b.bundle().pipe(through2());
+        ostream.pause();
         
-        b.bundle();
-        //b.bundle().pipe();
+        outer.on('package', function (pkg) {
+            var outdir = path.join(opts.dst, pkg.hash);
+            var tmpfile = path.join(outdir, '.bundle_' + pkg.hash + '.js');
+            var pending = 2, hash;
+            
+            mkdirp(outdir, function (err) {
+                if (err) return outer.emit('error', err);
+                
+                var h = crypto.createHash('sha1');
+                ostream.pipe(h).pipe(concat(function (buf) {
+                    hash = buf.toString('hex');
+                    done();
+                }));
+                ostream.pipe(h);
+                ostream.pipe(fs.createWriteStream(tmpfile))
+                    .on('close', done)
+                ;
+                ostream.resume();
+            });
+            
+            function done () {
+                if (--pending === 0) return;
+                var dstfile = path.join(outdir, 'bundle_' + hash + '.js');
+                fs.rename(tmpfile, dstfile, function (err) {
+                    if (err) outer.emit('error', err)
+                    else outer.emit('bundle.js', dstfile)
+                });
+            }
+        });
     });
     
     function withMap (map) {
@@ -41,7 +72,9 @@ module.exports = function (b, opts, cb) {
             var dir = pkg.package.__dirname || opts.basedir || process.cwd();
             var props = opts.keys || [];
             pkg.files = {};
-            pkg.dependencies = map.dependencies[key] || {};
+            pkg.path = dir;
+            pkg.dependencies = map.dependencies[key] || [];
+            pkg.hash = shasum(dir + '!' + pkg.dependencies.join(','));
             
             pkg.assets.forEach(function (file) {
                 props.forEach(function (prop) {
@@ -53,7 +86,6 @@ module.exports = function (b, opts, cb) {
                 });
             });
             
-            console.log(map);
             outer.emit('package', pkg);
         });
     }
