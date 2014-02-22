3babf8e780f3a04274f9ee3d4ac38b539404866f
James Halliday <mail@substack.net>
2014-02-21 16:30:41 -0800

    generate css bundles

--- index.js
+++ index.js
@@ -24,8 +24,16 @@ module.exports = function (b, opts) {
         
         outer.on('package', function (pkg) {
             var outdir = path.join(opts.dst, pkg.hash);
-            var tmpfile = path.join(outdir, '.bundle_' + pkg.hash + '.js');
-            var pending = 2, hash;
+            var tmpjs = path.join(outdir, '.bundle_' + pkg.hash + '.js');
+            var tmpcss = path.join(outdir, '.bundle_' + pkg.hash + '.css');
+            
+            var pending = 4, hashjs, hashcss;
+            var fileTypes = Object.keys(pkg.files).reduce(function (acc, key) {
+                pkg.files[key].forEach(function (file) {
+                    acc[file] = key;
+                });
+                return acc;
+            }, {});
             
             mkdirp(outdir, function (err) {
                 if (err) return outer.emit('error', err);
@@ -33,29 +41,57 @@ module.exports = function (b, opts) {
                 var p = pkg.package;
                 p.path = pkg.path;
                 if (!p.cartero) p.cartero = {};
-                console.log(pkg.files);
                 
                 var streams = packageWriter(p, pkg.files, outdir);
-                console.log(streams);
+                var types = {};
+                Object.keys(streams).forEach(function (key) {
+                    var t = fileTypes[key];
+                    if (!types[t]) types[t] = {};
+                    types[t][key] = streams[key];
+                });
                 
-                var h = crypto.createHash('sha1');
-                ostream.pipe(h).pipe(concat(function (buf) {
-                    hash = buf.toString('hex');
+                var cssBundle = through2();
+                var cssPending = 0;
+                
+                Object.keys(types.style || {}).forEach(function (key) {
+                    cssPending ++;
+                    var stream = types.style[key];
+                    stream.on('end', function () {
+                        if (-- cssPending === 0) cssBundle.push(null);
+                    });
+                    stream.pipe(cssBundle, { end: false });
+                });
+                
+                var hjs = crypto.createHash('sha1');
+                ostream.pipe(hjs).pipe(concat(function (buf) {
+                    hashjs = buf.toString('hex');
                     done();
                 }));
-                ostream.pipe(h);
-                ostream.pipe(fs.createWriteStream(tmpfile))
-                    .on('close', done)
-                ;
+                ostream.pipe(hjs);
+                ostream.pipe(fs.createWriteStream(tmpjs)).on('close', done);
+                
+                var hcss = crypto.createHash('sha1');
+                cssBundle.pipe(hcss).pipe(concat(function (buf) {
+                    hashcss = buf.toString('hex');
+                    done();
+                }));
+                cssBundle.pipe(fs.createWriteStream(tmpcss)).on('close', done);
+                
                 ostream.resume();
             });
             
             function done () {
-                if (--pending === 0) return;
-                var dstfile = path.join(outdir, 'bundle_' + hash + '.js');
-                fs.rename(tmpfile, dstfile, function (err) {
+                if (--pending !== 0) return;
+                var dstjs = path.join(outdir, 'bundle_' + hashjs + '.js');
+                var dstcss = path.join(outdir, 'bundle_' + hashcss + '.css');
+                
+                fs.rename(tmpjs, dstjs, function (err) {
                     if (err) outer.emit('error', err)
-                    else outer.emit('bundle.js', dstfile)
+                    else outer.emit('bundle.js', dstjs)
+                });
+                fs.rename(tmpcss, dstcss, function (err) {
+                    if (err) outer.emit('error', err)
+                    else outer.emit('bundle.css', dstcss)
                 });
             }
         });
