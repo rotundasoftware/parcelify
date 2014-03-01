605db6303eaad11d25026c01d019f22f359fa65e
James Halliday <mail@substack.net>
2014-02-28 23:48:41 -0800

    works with the latest parcel-map

--- example/copier.js
+++ example/copier.js
@@ -3,7 +3,8 @@ var browserify = require('browserify');
 var path = require('path');
 var file = path.resolve(process.argv[2]);
 
-copier(browserify(file), {
+var p = copier(browserify(file), {
     keys: [ 'style' ],
     dst: __dirname + '/dst'
 });
+p.on('package', console.log);
