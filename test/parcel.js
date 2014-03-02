104ed597e93b0a55f8ee19ea9e83b11d95e2f563
James Halliday <mail@substack.net>
2014-03-02 15:06:18 -0800

    failing test for an extra css file

--- test/parcel.js
+++ test/parcel.js
@@ -24,7 +24,8 @@ test('page1', function (t) {
             'bundle_0410bd9e41e2f1c21f670d3492f250c8f070eb3f.css',
             'bundle_bd37f2061a35a1fed52946ae31e4227d61c9eb4d.js',
             'view.html',
-            'x.css'  
+            'x.css',
+            'empty.css'
         ]);
     });
 });
